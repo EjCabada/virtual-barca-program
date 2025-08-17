cd0ebdaaf6fc8d5bbc47f9c7537a9e12bcff6be0
Isaac Chavez <chavez.isaac935@gmail.com>
2025-08-17 16:14:50 -0700

    Update BarcaBoard.java
    Add comments and clean up constructor by moving implementation of setBoard into it's own method

--- src/barcaBoardGame/BarcaBoard.java
+++ src/barcaBoardGame/BarcaBoard.java
@@ -1,25 +1,30 @@
 package barcaBoardGame;
-
 import java.awt.Point;
 
 public class BarcaBoard {
 	public static BarcaBoardSpace[][] board = new BarcaBoardSpace[10][10];
 	
 	public BarcaBoard() {
-		
+		setBoard();
+	}
+	
+	/**
+	 * Helper method to set the board to initial state
+	 */
+	public void setBoard() {
 		for(int i = 0; i < 10; i++) {
 			for(int j = 0; j < 10; j++) {
 				board[i][j] = new BarcaBoardSpace(new Point(i,j), false);
 			}
-		}	
-	}
-	
-	public void setBoard() {
+		}
 		board[0][0].setOccupyingPiece(new MousePiece());
 		// Test move validation
 		// board[0][0].getOccupyingPiece().isValidMove(new Point(0,0), new Point(0,9));
 	}
 	
+	/**
+	 * Printing the board object will implicitly call this method
+	 */	
 	public String toString() {
 		String string = "";
 		for(int i = 0; i < 10; i++) {
