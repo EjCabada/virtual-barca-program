1dec3e07a0edeb7e681ebdc84c5cc74cadcc4399
Isaac Chavez <chavez.isaac935@gmail.com>
2025-08-17 16:21:49 -0700

    Update BarcaPiece.java
    Implement basic validation method that applies to all pieces

--- src/barcaBoardGame/BarcaPiece.java
+++ src/barcaBoardGame/BarcaPiece.java
@@ -8,4 +8,20 @@ public abstract class BarcaPiece {
 	public abstract boolean isValidMove(Point from, Point to);
 	
 	public abstract String toString();
+	
+	/**
+	 * Determines if the move is valid for this piece
+	 * @param from the location of the piece intended to move
+	 * @param to the location of the destination of moved piece
+	 * @return true only if move satisfies the requirement of being within bounds
+	 */
+	public boolean isWithinBounds(Point from, Point to) {
+		boolean validX = from.x <= 10 && from.x >= 1;
+		boolean validY = from.y <= 10 && from.y >= 1;		
+		if(validX && validY) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
