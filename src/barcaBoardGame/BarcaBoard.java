69813f264fb6cc475e99d9103b74374a0ff8975b
Isaac Chavez <chavez.isaac935@gmail.com>
2025-08-18 19:15:04 -0700

    Implemented method for finding adjacent spaces
    Used brute force approach to get all adjacent spaces.

--- src/barcaBoardGame/BarcaBoard.java
+++ src/barcaBoardGame/BarcaBoard.java
@@ -18,6 +18,21 @@ public class BarcaBoard {
 		board[0][0].setOccupyingPiece(new MousePiece());
 	}
 	
+	public Point[] findAdjacentSpaces(Point startingPoint) {
+		int startingX = startingPoint.x;
+		int startingY = startingPoint.y;
+		
+		Point rightOfStart = new Point(startingX++, startingY);
+		Point leftOfStart = new Point(startingX--, startingY);
+		Point topOfStart = new Point(startingX, startingY++);
+		Point bottomOfStart = new Point(startingX, startingY--);
+		
+		Point topRightOfStart = new Point(startingX++, startingY--);
+		Point topLeftOfStart = new Point(startingX--, startingY--);
+		Point bottomRightOfStart = new Point(startingX++, startingY++);
+		Point bottomLeftOfStart = new Point(startingX--, startingY++);
+	}
+	
 	public void printBoard() {
 		for(int i = 0; i < 10; i++) {
 			for(int j = 0; j < 10; j++) {
